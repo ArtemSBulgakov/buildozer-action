283f634c0826dc8cdec93ebd7de938f9328d6b3f
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2020-06-23 15:01:06 +0300

    Fix home variable and owner of workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# GitHub sets root as owner of repository directory. Change it to user
+sudo chown -R "$USER" "$GITHUB_WORKSPACE"
+# GitHub sets HOME to /github/home, but Buildozer is installed to /home/user. Change HOME to user's home
+export HOME=$HOME_DIR
+
 # Run build
 sh -c "buildozer android debug"
+
+# Give access to root
+sudo chown -R root "$GITHUB_WORKSPACE"

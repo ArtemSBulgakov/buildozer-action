8fbb03b53f103449a6c6449e099f4fa574475b93
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2020-06-23 15:02:49 +0300

    Add Buildozer settings to disable interactions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,13 @@ sudo chown -R "$USER" "$GITHUB_WORKSPACE"
 # GitHub sets HOME to /github/home, but Buildozer is installed to /home/user. Change HOME to user's home
 export HOME=$HOME_DIR
 
+# Buildozer settings to disable interactions
+export BUILDOZER_WARN_ON_ROOT=0
+export APP_ANDROID_ACCEPT_SDK_LICENSE=1
+# Do not allow to change directories
+export BUILDOZER_BUILD_DIR=./.buildozer
+export BUILDOZER_BIN=./bin
+
 # Run build
 sh -c "buildozer android debug"
 
